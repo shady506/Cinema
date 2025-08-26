a5c4421046675b8176da038ab9cd70329652f67a
shady <shady@MSI>
2025-08-26 12:47:23 +0300

    Added New Feture To Project And Edit In Index Page And Details Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace Cinema.Controllers
 
 
 
+
+
+
+
         
         public IActionResult Index(FilteredMovies filteredMovies,int page = 1)
         {
@@ -40,15 +44,32 @@ namespace Cinema.Controllers
             Movies= Movies.Skip((page - 1) * 8).Take(8);
             return View(Movies.ToList());
         }
-
-
         public IActionResult Details(int Id)
         {
-            var movie = _context.Movies.Include(e => e.Cinema).Include(e => e.Category).FirstOrDefault(e => e.Id == Id);
+            var movie = _context.Movies.Include(e => e.Cinema).Include(e => e.Category).Include(e=>e.Actors).FirstOrDefault(e => e.Id == Id);
             if (movie == null)
                 return NotFound();
 
             return View(movie);
+        }
+        public IActionResult ActorDetails(int Id)
+        {
+            var actor = _context.Actors.Include(e=>e.Movies).FirstOrDefault(e => e.Id == Id);
+            if (actor is null)
+                return NotFound();
+
+            return View(actor);
+             
+        }
+        public IActionResult Categories(int Id)
+        {
+            var Categories = _context.Categories.AsQueryable();
+            return View(Categories.ToList());
+        }
+        public IActionResult Cinemas(int Id)
+        {
+            var Cinemas= _context.Cinemas.AsQueryable();
+            return View(Cinemas.ToList());
         }
 
 
